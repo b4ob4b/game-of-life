6d46fef12f8aea89370793392cae27ae8921d281
Julian Baumgärtner <ju.baumgaertner@gmail.com>
2019-10-20 21:03:58 +0200

    change state of cells

--- src/app/cellpad/cellpad.component.ts
+++ src/app/cellpad/cellpad.component.ts
@@ -5,17 +5,35 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./cellpad.component.html",
   styleUrls: ["./cellpad.component.css"]
 })
+const CELL_PAD_SIZE: number = 10;
+
 export class CellpadComponent implements OnInit {
   height: number[];
   width: number[];
+  livingCells: boolean[][];
 
   constructor() {
-    this.height = Array(10)
+    this.height = Array(CELL_PAD_SIZE)
       .fill(0)
       .map((x, i) => i);
-    this.width = Array(10)
+    this.width = Array(CELL_PAD_SIZE)
       .fill(0)
       .map((x, i) => i);
+
+    this.livingCells = [];
+
+    for (var i: number = 0; i < CELL_PAD_SIZE; i++) {
+      this.livingCells[i] = [];
+      for (var j: number = 0; j < CELL_PAD_SIZE; j++) {
+        this.livingCells[i][j] = false;
+      }
+    }
+  }
+
+  changeStatus(xCoordinate: number, yCoordinate: number): void {
+    this.livingCells[xCoordinate][yCoordinate] = !this.livingCells[xCoordinate][
+      yCoordinate
+    ];
   }
 
   ngOnInit() {}
